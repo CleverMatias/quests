d8eddb70f9eb6dc49cb7fd861ca7aeb1abca1fcc
Flávio Augusto da Silveira <flavioaugustosilveira@gmail.com>
2014-02-20 00:51:39 -0300

    update description of counting letters of words.

--- counting_letters_of_words.php
+++ counting_letters_of_words.php
@@ -1,4 +1,14 @@
 <?php
+/*
+Input is a string—a paragraph of text. One of the paragraphs above would be fine.
+
+Output is a report listing how many words there are with X letters, like:
+
+10 words with 1 letter
+20 words with 2 letters
+7 words with 3 letters
+15 words with 4 letters, etc.
+*/
 
 $input = "Simple code is critical. When I first started interviewing I'd ask people to solve hard problems. This didn't work. As Spolsky's famous The Guerrilla Guide to Interviewing argues, however, simple problems are just as good, or better. Simple problems test simple skills, and simple skills are the basis of complex ones. A programmer who struggles to loop through a bunch of words will be at sea performing far more complex tasks. If you can't boil water your Toad in the Hole is likely to come out wrong.";
 
